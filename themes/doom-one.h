4c68311146f5519c12772a3b5dfb10b9f409fe90
Kishore Satheeskumar <k.sath214@gmail.com>
2021-12-18 22:16:20 +0000

    Initial commit

---
+++ themes/doom-one.h
@@ -0,0 +1,31 @@
+/* Terminal colors (16 first used in escape sequence) */
+/* Colorscheme based on the 'Doom One' theme from Doom Emacs */
+static const char *colorname[] = {
+	/* 8 normal colors */
+	"#1c1f24",
+	"#ff6c6b",
+	"#98be65",
+	"#da8548",
+	"#51afef",
+	"#c678dd",
+	"#5699af",
+	"#202328",
+
+	/* 8 bright colors */
+	"#5b6268",
+	"#da8548",
+	"#4db5bd",
+	"#ecbe7b",
+	"#3071db",
+	"#a9a1e1",
+	"#46d9ff",
+	"#dfdfdf",
+
+	[255] = 0,
+
+	/* more colors can be added after 255 to use with DefaultXX */
+	"#282c34",
+	"#bbc2cf",
+	"#d7d7d7",
+};
+
